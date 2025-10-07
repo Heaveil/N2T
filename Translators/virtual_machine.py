1d6d043a31faf9c4afbeac8e5a6bf06172bf16ea
Heaveil <heaveilly@gmail.com>
2025-10-07 18:23:18 +1100

    call instruction

--- Translators/virtual_machine.py
+++ Translators/virtual_machine.py
@@ -124,7 +124,13 @@ def function_code(name, i, file_name):
 def call_code(name, i, function, file_name):
     global counter
     code = [
-        # Magic here
+        f"@{file_name}.{function}$ret.{counter}", "D=A", "@SP", "A=M", "M=D", "@SP", "M=M+1",
+        "@LCL", "D=M", "@SP", "A=M", "M=D", "@SP", "M=M+1",
+        "@ARG", "D=M", "@SP", "A=M", "M=D", "@SP", "M=M+1",
+        "@THIS", "D=M", "@SP", "A=M", "M=D", "@SP", "M=M+1",
+        "@THAT", "D=M", "@SP", "A=M", "M=D", "@SP", "M=M+1",
+        "@SP", "D=M", f"@{i}", "D=D-A", "@5", "D=D-A", "@ARG", "M=D",
+        "@SP", "D=M", "@LCL", "M=D",
         f"@{name}", "0;JMP",
         f"({file_name}.{function}$ret.{counter})"
     ]
