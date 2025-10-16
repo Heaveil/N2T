1e1cdfd94e02a3c19e6894bea27c1b9fb053ab9a
Heaveil <heaveilly@gmail.com>
2025-10-17 10:54:27 +1100

    tokenizer regex

--- Translators/compiler.py
+++ Translators/compiler.py
@@ -1,3 +1,7 @@
+import sys
+import os
+import re
+
 tokens_dict = {
     "keyword": [
         "class", "constructor", "function", "method", "field", "static", "var",
@@ -12,19 +16,41 @@ tokens_dict = {
 
 # Returns a Tuple -> (type, token)
 def tokenize(line):
-    line = "".join(line.split())
     tokens = []
+    escaped = [re.escape(s) for s in tokens_dict["symbol"]]
+    pattern = "(" + "|".join(escaped) + ")"
+    parts = re.split(pattern, line)
+    parts = [p.strip() for p in parts if p.strip()]
 
-    # TODO: 
-    # Iterate line char by char
-    # if char == comment:
-    #   continue
+    # TODO:
+    # split keywords without splitting string literals
+    # Remove comments
+    # comments either // .... or /* start (can be multiple lines) end */
+    return []
 
-    tokens += [("type", "token")]
-    return tokens
+    # for word in words :
+    #     if word in tokens_dict["keyword"]:
+    #         tokens += [("keyword", word)]
+    #     elif word in tokens_dict["symbol"]:
+    #         tokens += [("symbol", word)]
+    #     elif word.isdigit():
+    #         tokens += [("integerConstant", word)]
+    #     elif "\"" in word:
+    #         tokens += [("StringConstant", word.replace("\"", ""))]
+    #     else:
+    #         tokens += [("identifier", word)]
+    # return tokens
 
-import sys
-import os
+def write_token_file(tokens):
+    out_file = open("test.xml", "w")
+    out_file.write("<tokens>\n")
+    for token_tuple in tokens:
+        symbol, token = token_tuple[0], token_tuple[1]
+        out_file.write(f"<{symbol}>")
+        out_file.write(f" {token} ")
+        out_file.write(f"</{symbol}>\n")
+    out_file.write("</tokens>\n")
+    out_file.close()
 
 if __name__=="__main__":
     path = sys.argv[1]
@@ -37,12 +63,6 @@ if __name__=="__main__":
         lines = in_file.readlines()
         for line in lines:
             tokens += tokenize(line)
+        in_file.close()
 
-    out_file = open("test.xml", "w")
-    out_file.write("<tokens>\n")
-    for token_tuple in tokens:
-        symbol, token = token_tuple[0], token_tuple[1]
-        out_file.write(f"<{symbol}>")
-        out_file.write(f" {token} ")
-        out_file.write(f"</{symbol}>\n")
-    out_file.write("</tokens>\n")
+    write_token_file(tokens)
